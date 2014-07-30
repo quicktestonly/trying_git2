2eed9c0f873267ae7f800711bad4bdd2c6d485f4
coder collective <codercollective@gmail.com>
2014-07-30 08:14:08 -0700

    test only.  ignore

---
+++ trying_git2/src/trying_git/Trying_git2Servlet.java
@@ -0,0 +1,13 @@
+package trying_git;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Trying_git2Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
